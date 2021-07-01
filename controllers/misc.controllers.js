33abf3a3febd0fd2f1c85629493482efdcb7627e
Andrei <andrei_1002@hotmail.com>
2021-07-01 20:19:45 +0200

    implementada vista para crear drag

--- controllers/misc.controllers.js
+++ controllers/misc.controllers.js
@@ -26,5 +26,26 @@ module.exports.index = (req, res, next) => {
 
         res.render("detail", {drag: response.data})
       })
-      .catch ((error) => next(error))
+      .catch((error) => {
+        console.log(error)
+      })
+  };
+
+  module.exports.createDrag = (req, res, next) => {
+    res.render("new-drag")
+  }
+
+  module.exports.doCreateDrag = (req, res, next) => {
+    if (!req.body.name || req.body.name.length < 2) {
+      res.render('new-drag', {
+        drag: { name: req.body.name },
+        error: "Introduce a valid name"
+      })
+    } else {
+      http.post("/drags", req.body)
+      .then(() => {
+        res.redirect("/drags")
+      })
+      .catch((e) => console.log(e))
+    }
   }
--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
-const { Router } = require("express");
+// const { Router } = require("express");
 const miscController = require("../controllers/misc.controllers");
 
 router.get("/", miscController.index);
 
-router.get("/drags", miscController.drags)
-router.get("/drags/:id", miscController.getDrag)
+router.get("/drags", miscController.drags);
+router.get("/drag/new", miscController.createDrag);
+router.post("/drags", miscController.doCreateDrag);
+router.get("/drags/:id", miscController.getDrag);
 
 
-module.exports = router
+module.exports = router;
